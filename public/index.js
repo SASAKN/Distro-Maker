ae117563759983fd52e46ef6093e0d9a8366aa66
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-04-09 20:39:30 +0900

    Delete some js files and update index.html

--- public/index.js
+++
@@ -1,9 +0,0 @@
-//DistroMaker 0.01
-var {exec} = require('child_process');
-exec('pwd', (err, stdout, stderr) => {
-    if(err) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
