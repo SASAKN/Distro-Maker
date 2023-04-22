bc65734eff2bf48aa5a4b156a9640cb2cd13e850
SASAKEN <114722212+SASAKN@users.noreply.github.com>
2023-04-22 19:21:24 +0900

    Update

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 //必要なパッケージの読み込み
 let http = require('http');
-let fs = require('fs');
-var server = http.createServer(handler)
-const socket = require('socket.io');
+let express = require('express');
+const socketIO = require('socket.io');
 let { execSync } = require('child_process');
+//オブジェクトの作成
+const app = express();
+const server = http.Server( app );
+const io = socketIO( server );
 
 //----------------------------------------------------------
 //ここからソケットの処理
@@ -11,7 +14,6 @@ let { execSync } = require('child_process');
 
 
 //ソケット用のサーバーを起動。
-const io = socket( server );
 let usercount = 0; //ユーザー数
 //ソケットのコネクションを作成。
 io.on('connection', (socket) => {
@@ -48,64 +50,6 @@ io.on('connection', (socket) => {
 //----------------------------------------------------------
 //ここからサーバーの処理
 //----------------------------------------------------------
-
-//URLを取得して、ファイル形式に合わせた表示を行う
-function typeget(_url) {
-    //このサーバーで扱えるファイル形式
-    var types = {
-        ".html": "text/html",
-        ".css": "text/css",
-        ".js": "text/javascript",
-        ".png": "image/png",
-        ".gif": "image/gif",
-        ".svg": "svg+xml"
-    };
-    //タイプから見つけ出す
-    for (var type in types) {
-        if (_url.endsWith(type)) {
-            return types[type];
-        }
-    }
-    //もしそれになければプレーンテキストとして表示
-    return 'text/plain';
-}
-//リクエストを受信した時
-var server = http.createServer(function (req, res) {
-    //リクエストが、/で終わるならば、index.htmlをつける、そうでなければURLを表示。
-    var url = 'public' + (req.url.endsWith('/') ? req.url + 'index.html' : req.url);
-    //URLを表示
-    console.log(url);
-    //指定されたURLのファイルがあるのであれば
-    if (fs.existsSync(url)) {
-        //ファイルを読み込み、エラーとデータの変数に分ける。
-        fs.readFile(url, (err, data) => {
-            //エラーでなければ
-            if (!err) {
-                //正常に通信できたということを伝える。
-                res.writeHead(200, { 'Content-Type': typeget(url) });
-                res.end(data);
-                //ルーティングAPI呼び出し
-            } else if (url === 'public/api') {
-                res.writeHead(200, { 'Content-Type': typeget(url) });
-                res.end(data);
-                //DistroMakerのコア
-                execSync('sudo bash -c "./factory.sh"', (err, stdout, stderr) => {
-                    if (err) {
-                        console.log(`stderr: ${stderr}`);
-                        return;
-                    }
-                    console.log(`stdout: ${stdout}`);
-                    location.href = '../finish.html';
-                });
-            } else {
-                res.statusCode = 500;
-                res.end();
-            }
-        });
-    } else {
-        res.statusCode = 404;
-        res.end();
-    }
-});
+app.use( express.static( __dirname + '/public' ) );
 server.listen(1337, 'localhost');
 //APIをルーティングで作れば簡単に通信できる。
